5facf8ce189d18968968240498e825f2c66dca88
Manoj Bhamaraddi <manojbhamaraddi@gmail.com>
2025-06-28 14:53:14 +0530

    commit

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,14 +1,29 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// instance.interceptors.request.use((config) => {
+//   const stored = localStorage.getItem("user");
+//   const token = stored ? JSON.parse(stored).token : null;
+
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default instance;
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 instance.interceptors.request.use((config) => {
-  const stored = localStorage.getItem("user");
-  const token = stored ? JSON.parse(stored).token : null;
-
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
   return config;
 });
 
