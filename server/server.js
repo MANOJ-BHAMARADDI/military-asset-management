8455debc8cd6d15bf2c100011c5f9c19fa7f5035
Manoj Bhamaraddi <manojbhamaraddi@gmail.com>
2025-07-02 15:37:36 +0530

    new

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import path from "path";
 
 const PORT = process.env.PORT || 5000;
 
 connectDB(); 
+const __dirname = path.resolve();
+
+
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
